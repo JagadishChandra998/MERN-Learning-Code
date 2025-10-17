7b3a24a37e364adeb66e29acd7fd477a641268ae
Jagadihs Chandra Dhal <jagadishchandra998@gmail.com>
2025-10-17 17:28:29 +0530

    sorting and string

---
+++ class code/string.js
@@ -0,0 +1,37 @@
+// let a="Good /n morning";
+// let b="Hello" ;
+// let c =`I,
+// am,
+//  Jagadish`;
+
+// console.log(a);
+// console.log(b.length);
+// console.log(c);
+
+// let a="Hi";
+// let b = new String("Hi");
+// if(a==b){
+//     console.log(a);
+//     console.log(b);
+// }
+
+let a ="hello";
+let b="#Jagadishchandradhal#";
+
+// let c=b.slice(2,4);   
+// console.log(c)
+
+// let d=b.concat(" i am good");   
+// console.log(d)
+
+// let e=b.charAt(5);    // read the actual index
+// console.log(e)
+
+// let f=b.charCodeAt(3);    // read the unique value of the given index
+// console.log(f)
+
+// let g=b.codePointAt(0);   // GIVE THE NONEGATIVE POINTER VALUE
+// console.log(g)
+
+let h=b.at(2);     //GIVE THE INDEX VALUE
+console.log(h)
