aadb48ae5d80f7b031db26aee3407561ebfef4c3
Jagadihs Chandra Dhal <jagadishchandra998@gmail.com>
2025-10-17 10:05:14 +0530

    17/10

---
+++ class code/sorting.js
@@ -0,0 +1,34 @@
+// function main(){
+//     let arr =[1,6,8,4,5,6,2,5,110]
+//     arr.sort((a,b) =>a-b);
+
+//     process.stdout.write("sort");
+//     arr.forEach()
+
+// }
+
+function sort(arr,n){
+    var i=0;
+    while(i<n){
+        var correct =arr[i] -1;
+        if(arr[correct] !=arr[i]){
+            swap(arr,i,correct);
+        }
+        else{
+            i++
+        }
+    }
+}
+
+function swap(arr,i,correct){
+    var temp =arr[i];
+    arr[i] =arr[correct]
+    arr[correct]= temp;
+}
+var arr =[2,6,1,8,4,7,9]
+var n=7
+
+sort(arr,n);
+for(var i=0;i<n;i++){
+    console.log(arr[i]);
+}
